881bf751dfea57e33094883ffb5b6e0bb72b00d7
ellenupark <ellenupark@gmail.com>
2020-12-11 12:30:41 -0500

    Cleaned up code in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  prepareGame();
-})
-
 const gameForm = document.getElementById("form")
 const formName = document.getElementById("username")
 const playerURL = "http://localhost:3000/players"
@@ -10,62 +6,52 @@ const mainGameDiv = document.getElementById('main-game')
 const navBar = document.getElementById('nav-bar')
 const gameURL = "http://localhost:3000/games"
 
+document.addEventListener("DOMContentLoaded", function() {
+  prepareGame();
+});
+
 const prepareGame = async () => {
-  await API.loadPlayers()
-  debugger
-  // API.createCardSummary()
-  await Card.dealCards()
-  loadGame()
+  await API.loadPlayers();
+  await Card.dealCards();
+  loadGame();
 }
 
 function loadGame() {
-    gameForm.addEventListener("submit", function(event) {
-        event.preventDefault()
-        
-        // grab text from each field
-        const formResults = getInfo(event)
-        let options
-        let url
-        options = {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              "Accept": "application/json"
-            },
-            body: JSON.stringify(formResults)
-        }
-        url = gameURL;
+  gameForm.addEventListener("submit", function(event) {
+    event.preventDefault();
 
-        // fetch our results to the back end
-        fetch(url, options)
-        .then(resp => resp.json())
-        .then(newGame => {
-          if (!newGame.errors){
-            game.name = newGame.data.attributes.name
-            game.playGame();
-            gameForm.reset();
-          } else {
-            throw new Error( `${newGame.errors}`)
-          }
-        })
-        .then(data => revealBoard())
-        .catch(alert)
-    })  
-}
+    const formResults = getInfo(event);
+    let url = gameURL;
+    let options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formResults)
+    };
+
+    fetch(url, options)
+      .then(resp => resp.json())
+      .then(newGame => {
+        if (!newGame.errors){
+          game.name = newGame.data.attributes.name
+          game.playGame();
+          gameForm.reset();
+        } else {
+          throw new Error( `${newGame.errors}`)
+        }
+      })
+      .then(data => revealBoard())
+      .catch(alert);
+  });
+};
 
 function getInfo(event) {
   return {
     name: formName.value,
-  }
-}
-
-function createCardList() {
-    fetch(`http://localhost:3000/cards`)
-    .then(res => res.json())
-    .then(cards => {
-      createCardList(cards)
-    });
-}
+  };
+};
 
 function revealBoard() {
   document.getElementById('player-pairs').innerText = `${game.name}'s Sets`;
